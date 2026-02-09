97d270073753bcf1296cf8708d35744d65398d77
blackwellSW <pty0714@naver.com>
2026-02-09 11:54:48 +0900

    feat: API 키 제거

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,14 @@ import 'package:flutter/material.dart';
 import 'screens/landing_screen.dart';
 import 'screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // [수정] Firebase 초기화 시 옵션 직접 입력
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      // 1. Firebase 콘솔에서 복사한 '웹 API 키'를 여기에 붙여넣으세요
-      apiKey: "AIzaSyArhgKS-n0J1RDwOvmv4ufZLbMCAeUUN84", 
-      
-      // 2. Firebase 콘솔에서 복사한 '앱 ID'를 여기에 붙여넣으세요
-      appId: "1:84537953160:web:a31921d1e2f92958dc883d", 
-      
-      // 아래 2개는 제가 찾아서 이미 채워뒀습니다.
-      messagingSenderId: "84537953160", 
-      projectId: "knu-team-03",
-    ),
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp());
